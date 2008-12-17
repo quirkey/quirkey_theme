f357cfdb8ac288c0b8e995cbfa0c542e7b3e16dd
Aaron Quint <aaron@quirkey.com>
2008-12-17 15:20:07 -0500

    Integrating design into the php

--- footer.php
+++ footer.php
@@ -1,5 +1,27 @@
 			<div class="clear"></div>
 		</div>
+		<div id="bottom_holder">
+			<div id="bottom">
+				<div class="bottom_section">
+					<h3>About</h3>
+					<p class="tagline">QuirkeyBlog is<br /><strong>Aaron Quint's</strong><br /> perspective on the ongoing adventure of <br /><strong>Code</strong>, <strong class="a_lt">Life</strong>, <strong>Work</strong> <br />and the <strong class="a_lt">Web</strong>.</p>
+					<p>For more information check <a href="http://www.quirkey.com">Quirkey.com</a></p>
+				</div>
+				<div class="bottom_section">
+					<h3>Archives</h3>
+					<ul>
+						<?php wp_get_archives('type=monthly'); ?>
+					</ul>
+				</div>
+				<div class="bottom_section">
+					<h3>Categories</h3>
+					<ul>
+						<?php wp_list_cats('sort_column=name&optioncount=1&hierarchical=0'); ?>
+					</ul>
+				</div>
+				<div class="clear"></div>
+			</div>
+		</div>
 		<div id="footer">
 			<p>QuirkeyBlog is proudly powered by <a href="http://wordpress.org/">WordPress</a> <span>|</span> <a href="http://feeds.feedburner.com/quirkey">RSS</a></p>
 			<p class="quirkey_links">Quirkey<span>|</span><a href="http://www.quirkey.com" class="q_com">.com</a><span>|</span><a href="http://portfolio.quirkey.com" class="q_portfolio">Portfolio</a><span>|</span><a href="http://log.quirkey.com" class="q_log">Log</a><span>|</span><a href="http://www.quirkey.com/blog/" class="q_blog">Blog</a></p>
--- sidebar.php
+++ sidebar.php
@@ -1,95 +1,43 @@
-	<div id="sidebar">
-	
+<div id="sidebar">
+
+		<h3><a href="http://feeds.feedburner.com/quirkey"> <img src="<?php bloginfo('stylesheet_directory'); ?>/images/qblog-feed-icon20.png" alt="RSS" /> Subscribe via RSS</a>
+			<?php query_posts('showposts=4&offset=1')?>
+			<?php if (have_posts()) : ?>
+				<h3>Also Recent</h3>
+				<ul>
+				<?php while (have_posts()) : the_post(); ?>
+					<li>⚡ <a href="<?php the_permalink() ?>"><?php the_title(); ?></a><br /><?php the_time('F jS, Y') ?></li>
+				<?php endwhile; ?>
+
+			<?php endif; ?>
 		
-		<ul>
-			
-			<li>
-				<?php include (TEMPLATEPATH . '/searchform.php'); ?>
-			</li>
+		<?php /* If this is a 404 page */ if (is_404()) { ?>
+		<?php /* If this is a category archive */ } elseif (is_category()) { ?>
+		<p>You are currently browsing the archives for the <?php single_cat_title(''); ?> category.</p>
+		
+		<?php /* If this is a yearly archive */ } elseif (is_day()) { ?>
+		<p>You are currently browsing the <a href="<?php bloginfo('home'); ?>/"><?php echo bloginfo('name'); ?></a> weblog archives
+		for the day <?php the_time('l, F jS, Y'); ?>.</p>
+		
+		<?php /* If this is a monthly archive */ } elseif (is_month()) { ?>
+		<p>You are currently browsing the <a href="<?php bloginfo('home'); ?>/"><?php echo bloginfo('name'); ?></a> weblog archives
+		for <?php the_time('F, Y'); ?>.</p>
 
-			
+     <?php /* If this is a yearly archive */ } elseif (is_year()) { ?>
+		<p>You are currently browsing the <a href="<?php bloginfo('home'); ?>/"><?php echo bloginfo('name'); ?></a> weblog archives
+		for the year <?php the_time('Y'); ?>.</p>
+		
+	 <?php /* If this is a monthly archive */ } elseif (is_search()) { ?>
+		<p>You have searched the <a href="<?php echo bloginfo('home'); ?>/"><?php echo bloginfo('name'); ?></a> weblog archives
+		for <strong>'<?php echo wp_specialchars($s); ?>'</strong>. If you are unable to find anything in these search results, you can try one of these links.</p>
 
-			<!-- Author information is disabled per default. Uncomment and fill in your details if you want to use it.
-			<li><h2>Author</h2>
-			<p>A little something about you, the author. Nothing lengthy, just an overview.</p>
-			</li>
-			-->
+		<?php /* If this is a monthly archive */ } elseif (isset($_GET['paged']) && !empty($_GET['paged'])) { ?>
+		<p>You are currently browsing the <a href="<?php echo bloginfo('home'); ?>/"><?php echo bloginfo('name'); ?></a> weblog archives.</p>
 
-			<li>
-			<?php /* If this is a 404 page */ if (is_404()) { ?>
-			<?php /* If this is a category archive */ } elseif (is_category()) { ?>
-			<p>You are currently browsing the archives for the <?php single_cat_title(''); ?> category.</p>
-			
-			<?php /* If this is a yearly archive */ } elseif (is_day()) { ?>
-			<p>You are currently browsing the <a href="<?php bloginfo('home'); ?>/"><?php echo bloginfo('name'); ?></a> weblog archives
-			for the day <?php the_time('l, F jS, Y'); ?>.</p>
-			
-			<?php /* If this is a monthly archive */ } elseif (is_month()) { ?>
-			<p>You are currently browsing the <a href="<?php bloginfo('home'); ?>/"><?php echo bloginfo('name'); ?></a> weblog archives
-			for <?php the_time('F, Y'); ?>.</p>
-
-      <?php /* If this is a yearly archive */ } elseif (is_year()) { ?>
-			<p>You are currently browsing the <a href="<?php bloginfo('home'); ?>/"><?php echo bloginfo('name'); ?></a> weblog archives
-			for the year <?php the_time('Y'); ?>.</p>
-			
-		 <?php /* If this is a monthly archive */ } elseif (is_search()) { ?>
-			<p>You have searched the <a href="<?php echo bloginfo('home'); ?>/"><?php echo bloginfo('name'); ?></a> weblog archives
-			for <strong>'<?php echo wp_specialchars($s); ?>'</strong>. If you are unable to find anything in these search results, you can try one of these links.</p>
-
-			<?php /* If this is a monthly archive */ } elseif (isset($_GET['paged']) && !empty($_GET['paged'])) { ?>
-			<p>You are currently browsing the <a href="<?php echo bloginfo('home'); ?>/"><?php echo bloginfo('name'); ?></a> weblog archives.</p>
-
-			<?php } ?>
-			</li>
-			
-			<li><a href="feed:http://feeds.feedburner.com/quirkey"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/feed-icon-long-blue.png" alt="Subscribe via RSS" /></a></li>
-			
-			<li><h2>Quirkey</h2>
-				<ul>
-				<li>Software</li>
-				<li><a href="http://www.quirkey.com/about.html">About Quirkey</a></li>
-				</ul>
-			</li>
-			
-			
-			
-			<!-- <li><h2>Feed Me <img src="<?php bloginfo('stylesheet_directory'); ?>/images/qblog-feed-icon16.png" alt="feed icon" align="bottom" /></h2>
-				<ul>
-					<li><a href="feed:http://feeds.feedburner.com/quirkey">All Entries (RSS2)</a></li>
-					<li><a href="feed:http://feeds.feedburner.com/quirkeyblog-geek">foRGeeKsOnLy (RSS2)</a></li>
-					<li><a href="feed:http://feeds.feedburner.com/quirkeyblog-life">The Quirkey Life (RSS2)</a></li>
-					<li><a href="feed:<?php bloginfo('comments_rss2_url'); ?>">Comments (RSS2)</a></li>
-					<li><br /></li>
-					<li><a href="http://www.quirkey.com/blog/2006/04/17/now-with-more-or-less-geek/">Why so many choices?</a></li>
-				</ul>
-			</li>	 -->
-			
-			<?php
-			//hide this temporarily
-			//<!-- <li><h2>Listening</h2>
-			//	<?php get_scrobbler();
-			//</li> -->
-			?>
-			<?php wp_list_pages('title_li=<h2>Pages</h2>' ); ?>
-
-			<li><h2>Archives</h2>
-				<ul>
-				<?php wp_get_archives('type=monthly'); ?>
-				</ul>
-			</li>
-
-			<li><h2>Categories</h2>
-				<ul>
-				<?php wp_list_cats('sort_column=name&optioncount=1&hierarchical=0'); ?>
-				</ul>
-			</li>
-
-			<?php /* If this is the frontpage */ if ( is_home() || is_page() ) { ?>				
-				<?php get_links_list(); ?>
+		<?php } ?>
+		
 				
-			
-			<?php } ?>
-			
-		</ul>
-	</div>
+		<?php include (TEMPLATEPATH . '/searchform.php'); ?>
+	
+</div>
 
