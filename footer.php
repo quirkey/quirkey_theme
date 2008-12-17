ee3d73d2abd6a0205c302a463299f36b78ad365d
Aaron Quint <aaron@quirkey.com>
2008-12-17 12:35:23 -0500

    Re-did header and footer

--- footer.php
+++ footer.php
@@ -1,19 +1,25 @@
+			<div class="clear"></div>
+		</div>
+		<div id="footer">
+			<p>QuirkeyBlog is proudly powered by <a href="http://wordpress.org/">WordPress</a> <span>|</span> <a href="http://feeds.feedburner.com/quirkey">RSS</a></p>
+			<p class="quirkey_links">Quirkey<span>|</span><a href="http://www.quirkey.com" class="q_com">.com</a><span>|</span><a href="http://portfolio.quirkey.com" class="q_portfolio">Portfolio</a><span>|</span><a href="http://log.quirkey.com" class="q_log">Log</a><span>|</span><a href="http://www.quirkey.com/blog/" class="q_blog">Blog</a></p>
+			<p id="copy">quirkey logo and identity, &copy; 2005-2008 Quirkey NYC, LLC</p>
+		</div>
+	</div>
+  <script type="text/javascript">
+	$(function () {
+		Quirkey.initialize();
+	});
+	</script>
+ 	<script type="text/javascript">
+		var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+		document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+		</script>
+		<script type="text/javascript">
+		var pageTracker = _gat._getTracker("UA-3763169-1");
+		pageTracker._initData();
+		pageTracker._trackPageview();
+	</script>
 
-</div>
-<div style="clear:both;">&nbsp;</div>
-</div>
-<hr />
-<div id="footer">
-	<p>
-		<?php bloginfo('name'); ?> is proudly powered by 
-		<a href="http://wordpress.org/">WordPress</a>
-		<br /><a href="feed:<?php bloginfo('rss2_url'); ?>">Entries (RSS)</a>
-		and <a href="feed:<?php bloginfo('comments_rss2_url'); ?>">Comments (RSS)</a>.
-		<!-- <?php echo get_num_queries(); ?> queries. <?php timer_stop(1); ?> seconds. -->
-	</p>
-</div>
-
-
-		<?php wp_footer(); ?>
 </body>
 </html>
--- header.php
+++ header.php
@@ -1,38 +1,42 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
-	"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 
-<head profile="http://gmpg.org/xfn/11">
-<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
+	<meta name="generator" content="WordPress <?php bloginfo('version'); ?>" /> <!-- leave this for stats -->
 
-<title><?php bloginfo('name'); ?> <?php if ( is_single() ) { ?> &raquo; Blog Archive <?php } ?> <?php wp_title(); ?></title>
-
-<meta name="generator" content="WordPress <?php bloginfo('version'); ?>" /> <!-- leave this for stats -->
-
-<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
-<link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/mate.css" type="text/css" media="screen" />
-<link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS Feed" href="http://feeds.feedburner.com/quirkey" />
-<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
-
-
-<?php wp_head(); ?>
+	<title><?php bloginfo('name'); ?> <?php if ( is_single() ) { ?> &raquo; Blog Archive <?php } ?> <?php wp_title(); ?></title>
+		
+	
+	<link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/stylesheets/quirkey_theme.css" type="text/css" media="screen" charset="utf-8"true>
+	<link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/stylesheets/quirkey.css" type="text/css" media="screen" charset="utf-8"true>
+	<link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/mate.css" type="text/css" media="screen" />
+	
+	<script src="<?php bloginfo('stylesheet_directory'); ?>/javascripts/jquery.min.js" type="text/javascript" charset="utf-8"></script>
+	<script src="<?php bloginfo('stylesheet_directory'); ?>/javascripts/jquery.livequery.min.js" type="text/javascript" charset="utf-8"></script>
+	<script src="<?php bloginfo('stylesheet_directory'); ?>/javascripts/quirkey.js" type="text/javascript" charset="utf-8"></script>
+	
+	<link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS Feed" href="http://feeds.feedburner.com/quirkey" />
+	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+	
+	<?php wp_head(); ?>
+	
 </head>
+
 <body>
-<div id="container">
-<div id="logo">
-<a href="<?php echo get_settings('home'); ?>/">
-<img src="<?php bloginfo('stylesheet_directory'); ?>/images/qbloglogo.gif" alt="QuirkeyBlog" /></a>
-<?php get_sidebar(); ?>
-</div>
-<div id="page">
-<div id="header">
-		<h1><a href="<?php echo get_settings('home'); ?>/">Quirkey<span class="page_title">Blog</span></a></h1>
-			<?php /*<div id="flickr">
-				<ul><?php get_flickrrss(5, "user", "", "square", "<li>", "</li>", "32169282@N00"); ?></ul><br />
-				<div class="clear">&nbsp;</div>
-			</div> */ ?>
-			
-	<!-- 	<div class="description"><?php witty(); ?></div> -->
-</div>
-
-
+	<div id="container">
+		<div id="header_holder">
+			<div id="header">
+				<div id="logo" class="quirkey_nav_open"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/logo.png" alt="Logo"/></div>
+				<h1>Quirkey<a href="<?php echo get_settings('home'); ?>/" class="q_blog">Blog</a><span class="description">Various Writing.</span></h1>
+        <!-- <div id="est"><em>Est. 2005</em></div> -->
+				<div id="quirkey_nav" style="display: none;">
+				  <h1 class="quirkey_log">Quirkey<a href="http://log.quirkey.com/" class="q_log">Log</a><span class="description">Up-to-the-minute updates from the interweb.</span></h1>
+				  <h1 class="quirkey_blog">Quirkey<a href="http://portfolio.quirkey.com/" class="q_portfolio">Portfolio</a><span class="description">Selected Work.</span></h1>
+				  <h1 class="quirkey_com">Quirkey<a href="http://www.quirkey.com" class="q_com">.com</a><span class="description">The Lobby; General Information.</span></h1>
+				</div>
+			</div>
+			<div class="quirkey_nav_close" style="display:none;"><a href="#">^</a></div>
+		</div>
+		<div id="main">
